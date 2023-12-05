02fa944838aff3d92a70ed9f1ef3eb95fc0daa73
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-12-05 10:24:40 +0100

    reformat code to comply with our coding standards

--- core/src/main/java/io/wcm/testing/mock/aem/MockDataType.java
+++ core/src/main/java/io/wcm/testing/mock/aem/MockDataType.java
@@ -1,35 +1,37 @@
 package io.wcm.testing.mock.aem;
 
-import com.adobe.cq.dam.cfm.DataType;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import com.adobe.cq.dam.cfm.DataType;
+
 public class MockDataType implements DataType {
 
-    boolean isArray;
+  boolean isArray;
 
-    public MockDataType(boolean isArray) {
-        this.isArray = isArray;
-    }
+  public MockDataType(boolean isArray) {
+    this.isArray = isArray;
+  }
 
-    public @Nullable String getSemanticType() {
-        return StringUtils.EMPTY;
-    }
+  public @Nullable String getSemanticType() {
+    return StringUtils.EMPTY;
+  }
 
-    @Override
-    public boolean isMultiValue() {
-        return isArray;
-    }
+  @Override
+  public boolean isMultiValue() {
+    return isArray;
+  }
 
-    // --- unsupported operations ---
+  // --- unsupported operations ---
 
-    @Override
-    public @NotNull String getTypeString() {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public @NotNull String getTypeString() {
+    throw new UnsupportedOperationException();
+  }
 
-    public @NotNull String getValueType() {
-        throw new UnsupportedOperationException();
-    }
+  public @NotNull String getValueType() {
+    throw new UnsupportedOperationException();
+  }
+
 }
--- core/src/main/java/io/wcm/testing/mock/aem/MockFragmentData.java
+++ core/src/main/java/io/wcm/testing/mock/aem/MockFragmentData.java
@@ -1,64 +1,67 @@
 package io.wcm.testing.mock.aem;
 
-import com.adobe.cq.dam.cfm.ContentFragmentException;
-import com.adobe.cq.dam.cfm.DataType;
-import com.adobe.cq.dam.cfm.FragmentData;
+import java.util.Calendar;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Calendar;
+import com.adobe.cq.dam.cfm.ContentFragmentException;
+import com.adobe.cq.dam.cfm.DataType;
+import com.adobe.cq.dam.cfm.FragmentData;
 
 public class MockFragmentData implements FragmentData {
 
-    private Object value;
-    private String contentType;
-    private final MockDataType mockDataType;
+  private Object value;
+  private String contentType;
+  private final MockDataType mockDataType;
 
-    public MockFragmentData(Object value, boolean isArray) {
-        this.value = value;
-        this.mockDataType = new MockDataType(isArray);
-    }
+  public MockFragmentData(Object value, boolean isArray) {
+    this.value = value;
+    this.mockDataType = new MockDataType(isArray);
+  }
 
-    @Override
-    public @NotNull DataType getDataType() {
-        return mockDataType;
-    }
+  @Override
+  public @NotNull DataType getDataType() {
+    return mockDataType;
+  }
 
-    @Override
-    public <T> @Nullable T getValue(Class<T> type) {
-        if (type.isInstance(value)) {
-            return (T) value;
-        } else {
-            return null;
-        }
+  @Override
+  public <T> @Nullable T getValue(Class<T> type) {
+    if (type.isInstance(value)) {
+      return (T)value;
     }
+    else {
+      return null;
+    }
+  }
 
-    @Override
-    public boolean isTypeSupported(Class type) {
-        return type.isInstance(value);
-    }
+  @Override
+  public boolean isTypeSupported(Class type) {
+    return type.isInstance(value);
+  }
 
-    @Override
-    public @Nullable Object getValue() {
-        return value;
-    }
+  @Override
+  public @Nullable Object getValue() {
+    return value;
+  }
 
-    @Override
-    public void setValue(@Nullable Object value) throws ContentFragmentException {
-        this.value = value;
-    }
+  @Override
+  public void setValue(@Nullable Object value) throws ContentFragmentException {
+    this.value = value;
+  }
 
-    @Override
-    public @Nullable String getContentType() {
-        return contentType;
-    }
+  @Override
+  public @Nullable String getContentType() {
+    return contentType;
+  }
 
-    @Override
-    public void setContentType(@Nullable String contentType) {
-        this.contentType = contentType;
-    }
+  @Override
+  public void setContentType(@Nullable String contentType) {
+    this.contentType = contentType;
+  }
 
-    public @Nullable Calendar getLastModified() {
-        return null;
-    }
+  public @Nullable Calendar getLastModified() {
+    return null;
+  }
+
 }
